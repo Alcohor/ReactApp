3b001d2605baf23a82283a8509fe8508fc0ca580
alcohor <1013674928@qq.com>
2018-11-28 23:05:56 +0800

    clean demo2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import {Route,BrowserRouter,Switch,Redirect} from 'react-router-dom'
 import Detail from './pages/detail'
 import Home  from '@pages/home'
